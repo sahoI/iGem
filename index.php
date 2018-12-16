aebbaeed539db30679f2358770f25d14e8bc096e
sahoI <saho0003@gmail.com>
2018-12-16 21:30:11 +0900

    試し

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
  * @var bool
  */
 include('index.html');
-echo 'Hello World';
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+//echo 'Hello World';
+//define('WP_USE_THEMES', true);
+//
+///** Loads the WordPress Environment and Template */
+//require( dirname( __FILE__ ) . '/wp-blog-header.php' );
