e87ada41b2946a96b4d81529b9c65a9624d18f8f
sahoI <saho0003@gmail.com>
2019-03-28 13:22:37 +0900

    現在の状態

--- js/media_query.js
+++ js/media_query.js
@@ -1,5 +1,12 @@
-if (navigator.userAgent.indexOf("iPhone") > 0 || navigator.userAgent.indexOf("iPod") > 0 || (navigator.userAgent.indexOf("Android") > 0 && navigator.userAgent.indexOf("Mobile") > 0 )) {
-    document.getElementById("media").innerHTML = "スマートフォンからアクセス";
-} else {
-    document.getElementById("media").innerHTML = "PCからアクセス";
-}
+document.getElementById( "panel-btn" ).addEventListener( "click", function(){
+    console.log(document.getElementById( "panel-btn-icon" ).classList.contains( "close" ));
+    // パネルを閉じる
+    if ( document.getElementById( "panel-btn-icon" ).classList.contains( "close" ) ){
+        document.getElementById( "g-navi" ).style.right = "100%";
+        document.getElementById( "panel-btn-icon" ).classList.remove( "close" );
+        // パネルを開く
+    } else {
+        document.getElementById( "g-navi" ).style.right = 0;
+        document.getElementById( "panel-btn-icon" ).classList.add( "close" );
+    }
+} );
